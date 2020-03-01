e975fc7ddb09ba6a343f1eeb5681eb285a157b23
Gilberto Leandro <codexfast@gmail.com>
2020-03-01 13:27:04 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,18 @@
 |
 */
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin', 'AdminController@index');
+    Route::get('/admin/logout', function (Request $request) {
+        $request->session()->forget('admin_on');
+        
+        return redirect('/admin');
+    });
 });
 
 Route::get('/login', 'LoginController@index');
 Route::post('/login', 'LoginController@check');
+
