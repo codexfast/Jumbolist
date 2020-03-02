06953f4e5e524cf994701d715fc8cf018bc5dd8d
Gilberto Leandro <codexfast@gmail.com>
2020-03-01 23:10:49 -0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,19 @@ use Illuminate\Support\Facades\Redirect;
 
 Route::middleware(['admin'])->group(function () {
     Route::get('/admin', 'AdminController@index');
+    Route::get('/admin/settings', 'AdminController@settings');
+
+    Route::delete('/admin/donate/{id}', 'DonateController@delete')->where(['id' => '[0-9]+']);
+    Route::post('/admin/donate', 'DonateController@create');
+
     Route::get('/admin/logout', function (Request $request) {
         $request->session()->forget('admin_on');
         
         return redirect('/admin');
     });
 });
+
+Route::get('/', 'HomeController@index');
 
 Route::get('/login', 'LoginController@index');
 Route::post('/login', 'LoginController@check');
