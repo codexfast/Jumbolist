21053491e5e96090d89a58206d24fce8f4906ced
Gilberto Leandro <codexfast@gmail.com>
2020-03-01 23:09:33 -0300

    Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Donate;
 
 class AdminController extends Controller
 {
     public function index(Request $request)
     {
-        return view('admin');
+        return view('dashboard');
+    }
+
+    public function settings(Request $request)
+    {
+        
+        return view('settings', [
+            'donates' => Donate::all()
+            
+        ]);
     }
 }
