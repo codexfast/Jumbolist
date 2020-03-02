4d627c3ad9309e57f9e7d4278314ec4d38e94fe4
Gilberto Leandro <codexfast@gmail.com>
2020-03-02 14:30:16 -0300

    Admin done

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -14,19 +14,21 @@
         <div class="card mb-4">
             <div class="card-header"><i class="fas fa-envelope mr-1"></i>Configurações de E-mail e Nome</div>
             <div class="card-body">
-                <form>
+                <form method="POST" action="{{url('/admin/profile')}}">
+                    <input type="hidden" name="_method" value="PUT">
+                    @csrf
                     <div class="form-group">
                         <label for="in_name">Nome</label>
-                        <input type="text" class="form-control" id="in_name" aria-describedby="emailHelp" placeholder="Novo Nome" name="name">
+                    <input type="text" class="form-control" id="in_name" aria-describedby="emailHelp" placeholder="Novo Nome" name="name" required value="{{$admin->name}}">
                     </div>
                     <div class="form-group">
                         <label for="in_email">E-mail</label>
-                        <input type="email" class="form-control" id="in_email" aria-describedby="emailHelp" placeholder="Novo E-mail" name="email">
+                    <input type="email" class="form-control" id="in_email" aria-describedby="emailHelp" placeholder="Novo E-mail" name="email" required value="{{$admin->email}}">
                         <small id="emailHelp" class="form-text text-muted">Esta mudança afetará no login</small>
                     </div>
                     <div class="form-group">
                         <label for="in_password">Password</label>
-                        <input type="password" class="form-control" id="in_password" placeholder="Confirme Senha" name="password">
+                        <input type="password" class="form-control" id="in_password" placeholder="Confirme Senha" name="password" required>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Salvar</button>
@@ -39,21 +41,22 @@
             <div class="card-header"><i class="fas fa-key mr-1"></i>Configurações
                 de Senha</div>
             <div class="card-body">
-                <form>
-
+                <form method="post" action="{{url('/admin/password')}}">
+                    <input type="hidden" name="_method" value="PUT">
+                    @csrf
                     <div class="form-group">
                         <label for="in_password">Antiga senha</label>
-                        <input type="password" class="form-control" id="in_password"  name="old_password">
+                        <input type="password" class="form-control" id="in_password"  name="old_password" required>
                     </div>
 
                     <div class="form-group">
                         <label for="in_repassword">Nova senha</label>
-                        <input type="password" class="form-control" id="in_repassword"  name="new_password">
+                        <input type="password" class="form-control" id="in_repassword"  name="new_password" required>
                     </div>
 
                     <div class="form-group">
                         <label for="in_repassword">Repita nova senha</label>
-                        <input type="password" class="form-control" id="in_repassword"  name="new_repassword">
+                        <input type="password" class="form-control" id="in_repassword"  name="new_password_confirmation" required>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Salvar</button>
@@ -71,7 +74,7 @@
                     <h4 class="alert-heading">Simples explicação</h4>
                     <p>Para obter o link de pagamento entre em <a href="https://www.mercadopago.com.br/tools/create">Mercado Pago Tool Create</a> logado com sua conta que ira receber o pagamento.</p>
                     <hr>
-                    <p class="mb-0">Ao entrar preencher os campos obrigatórios, o seguinte é abrir a seção <strong>Mais opções</strong> e preencher os com os dados abaixo  </p>
+                    <p class="mb-0">Ao entrar preencher os campos obrigatórios, o seguinte é abrir a seção <strong>Mais opções</strong> e preenchê-los com os dados abaixo  </p>
                 </div>
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
