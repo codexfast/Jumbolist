ecb637286beb62ca268919e14f95bac2c1e03ebb
Gilberto Leandro <codexfast@gmail.com>
2020-03-02 14:29:57 -0300

    add 'thanks' page

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="refresh" content="5;url={{url('')}}" />
+    <title>Obrigado pela doação</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  </head>
+  <body>
+    <div class="container py-4">
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Obrigado!</h4>
+            <p>Sua doação será de grande ajuda para o desenvolvimento da plataforma.</p>
+            <hr>
+        <p class="mb-0">Caso não tenha sido redirecionado para pagina principal, <strong><a href="{{url('')}}">click aqui</a></strong></p>
+        </div>
+    </div>
+
+  </body>
+</html>
