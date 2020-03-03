2071cad5b33afa02f3459032ebd68fffce31538a
Gilberto Leandro <codexfast@gmail.com>
2020-03-02 22:28:36 -0300

    Start manage options

---
+++ public/js/dashboard.js
@@ -0,0 +1,21 @@
+(function () {
+    
+    states_cities.estados.map(state => {
+        $('#selectState').append(new Option(state.nome, state.sigla))
+    });
+
+    $('#selectState').change(e => {
+        let sigla = $('#selectState').children("option:selected").val();
+
+
+        state = states_cities.estados.filter(state => state.sigla == sigla);
+
+        if (state.length === 1)
+        {
+            state[0].cidades.map((city, index) => {
+                $('#selectCity').empty();
+                $('#selectCity').append(new Option(city, index))
+            });
+        }
+    });
+})();
