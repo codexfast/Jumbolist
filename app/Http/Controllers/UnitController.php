75ba6a632851fff9424c3b75eea6128976acc26b
Gilberto Leandro <codexfast@gmail.com>
2020-03-06 10:34:35 -0300

    Finished

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -63,4 +63,37 @@ class UnitController extends Controller
         return redirect('/admin')->with('success', 'Aceito com sucesso!');
 
     }
+
+    public function client_create(Request $request)
+    {
+
+        
+        $request->validate([
+            'state' => 'required',
+            'city' => 'required',
+            'unit' => 'required|min:1',
+            'fileList' => 'required|mimes:pdf,jpg,png,jpeg|max:10000'
+        ]);
+        
+        $state = $request->input('state');
+        $city = $request->input('city');
+        $unity = $request->input('unit');
+        $file = null;
+
+        if (!is_null($request->fileList))
+        {
+            $file = time().'-list.'.$request->fileList->extension(); 
+            $request->fileList->move(public_path('files'), $file);
+        }
+
+        Unit::create([
+            'unit' => $unity,
+            'initials' => $state,
+            'city' => $city,
+            'list' => $file,
+            'pendent' => true
+        ]);
+    
+        return redirect('/buscar')->with('success', 'Unidade criada!');
+    }
 }
