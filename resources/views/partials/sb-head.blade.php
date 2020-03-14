26aaecf83e710037c085e671061c0573c21a66ef
Gilberto Leandro <codexfast@gmail.com>
2020-03-14 13:46:06 -0300

    fix

--- resources/views/partials/sb-head.blade.php
+++ resources/views/partials/sb-head.blade.php
@@ -1,2 +1,2 @@
-<link href="{{asset("/sb/dist/css/styles.css")}}" rel="stylesheet" />
+<link href="{{secure_asset("/sb/dist/css/styles.css")}}" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
